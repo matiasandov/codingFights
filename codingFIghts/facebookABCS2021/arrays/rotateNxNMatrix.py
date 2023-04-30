c8b85326efd78b7c35730938f2ef09d6cb33817d
Matías Méndez <58478529+matiasandov@users.noreply.github.com>
2023-04-30 22:48:50 +0000

    Create rotateNxNMatrix.py

---
+++ codingFIghts/facebookABCS2021/arrays/rotateNxNMatrix.py
@@ -0,0 +1,11 @@
+# reverse
+l = 0
+r = len(matrix) -1
+while l < r:
+	matrix[l], matrix[r] = matrix[r], matrix[l]
+	l += 1
+	r -= 1
+# transpose 
+for i in range(len(matrix)):
+	for j in range(i):
+		matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
