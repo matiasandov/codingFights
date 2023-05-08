cf8a38a5dac818ca5a495a38eb8a46ce68a7be98
Matías Méndez <58478529+matiasandov@users.noreply.github.com>
2023-05-08 16:44:06 +0200

    Update rotateNxNMatrix.py

--- codingFIghts/facebookABCS2021/arrays/rotateNxNMatrix.py
+++ codingFIghts/facebookABCS2021/arrays/rotateNxNMatrix.py
@@ -1,11 +1,28 @@
-# reverse
-l = 0
-r = len(matrix) -1
-while l < r:
-	matrix[l], matrix[r] = matrix[r], matrix[l]
-	l += 1
-	r -= 1
-# transpose 
-for i in range(len(matrix)):
-	for j in range(i):
-		matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+#rotar clockwise
+def rotateMatrix(mat):
+    #first row
+    p =0
+    #ultima row -> siempre -1 
+    u = len(mat) -1
+    
+    #reverse matrix
+    #intercambia hasta que se encuentren en la mitad de la matriz, funciona aunque sea par o impar mientras 
+    #u > p
+    while u > p:
+        mat[p],mat[u] = mat[u],mat[p]
+        p += 1
+        u -= 1
+    
+    #transpose
+    for i in range(len(mat)):
+        for j in range(i):
+            mat[i][j],mat[j][i] = mat[j][i],mat[i][j]
+            
+    
+    #si quiero rotar anticlockwise haces reverse de nuevo
+    # while u > p:
+    #     mat[p],mat[u] = mat[u],mat[p]
+    #     p += 1
+    #     u -= 1
+        
+    return mat
