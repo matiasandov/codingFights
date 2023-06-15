72d6b9e388c6eb186eaeb8760a0a5a6dc28beb8c
Matías Méndez <58478529+matiasandov@users.noreply.github.com>
2023-06-15 03:27:38 +0200

    Update longestSubstring.py

--- codingFIghts/facebookABCS2021/arrays/longestSubstring.py
+++ codingFIghts/facebookABCS2021/arrays/longestSubstring.py
@@ -1,34 +1,39 @@
+"""
+Ejemplo: s = "dvdf"
+Necesitas guardar los indices de donde viste la ultima aparición de cada caracter
+Si lo haces con solo un for sin guardar indices cuando se encuentre la segunda "d" solo quedará guardado como máximo "df"
+cuando en realidad el más largo es "vdf" pero cuando estes en la segunda "d" tienes que retroceder a "v" para encontrar longitud máxima 
 
-def longestUniqueSubsttr(str):
-    
-    n = len(str)
-    
-    # Result
-    res = 0
+"""
 
-    for i in range(n):
-        
-        # marcar que letras ya visito
-        visited = [0] * 256  
 
-        for j in range(i, n):
-        #va a comparir i contra todos los otros valores siguentes
-            # If current character is visited
-            # Break the loop
-            if (visited[ord(str[j])] == True):
-                break
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
 
-            # Else update the result if
-            # this window is larger, and mark
-            # current character as visited.
-            else:
-                res = max(res, j - i + 1)
-                visited[ord(str[j])] = True
-        
-        # Remove the first character of previous
-        # window
-        # si encuentra dos repetidos, excluira como la primera 
-        #aparicion del repetidoe
-        visited[ord(str[i])] = False
-    
-    return res
+        # Diccionario donde guardarás el index más reciente de cada elemento
+        char_map = {}
+        max_length = 0
+        #index de incio de la max length
+        start = 0
+
+        #iteras accediendo a indice para poder guardarlo en lugar de for i in s
+        for end in range(len(s)):
+            # si ya se repitio el elemento en posicion "end", guardas la posición siguiente de la ultima vez que se encontro
+            # es decir en "dvdf" se guardaria la posición [1] de s[1] = "v"
+            if s[end] in char_map:
+                start = max(start, char_map[s[end]] + 1)
+
+            #guardas la última posicion vista (posicion actual)  del caracter actual
+            char_map[s[end]] = end
+
+            # calculas length actual +1 porque esta indexada en 0
+            length = end - start + 1
+
+            # actualizas la length maxima
+            max_length = max(max_length, length)
+
+        return max_length
